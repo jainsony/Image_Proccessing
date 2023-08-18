9ee18cc0d84d3515f057c001320fdbf81e4114f5
Enigma bell <54368594+jainsony@users.noreply.github.com>
2023-08-18 16:48:50 +0530

    self driving virtual environment

---
+++ NFS_SAMPLES/test2.py
@@ -0,0 +1,70 @@
+import cv2
+import numpy as np
+
+def nothing(x):
+    pass
+
+# cv2.namedWindow("Tracking")
+# cv2.createTrackbar("LH", "Tracking", 0, 255, nothing)
+# cv2.createTrackbar("LS", "Tracking", 0, 255, nothing)
+# cv2.createTrackbar("LV", "Tracking", 0, 255, nothing)
+# cv2.createTrackbar("UH", "Tracking", 255, 255, nothing)
+# cv2.createTrackbar("US", "Tracking", 255, 255, nothing)
+# cv2.createTrackbar("UV", "Tracking", 255, 255, nothing)
+
+vidcap = cv2.VideoCapture("nfs1.mp4")
+# Capture video frame by frame
+success, frame = vidcap.read()
+
+
+while True:
+
+    # Capture video frame by frame
+    success, frame = vidcap.read()
+
+    # Resize the image frames
+    resize = cv2.resize(frame, (640, 480))
+    # frame = cv2.imread('nfs3.png')
+
+    hsv = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
+
+    # l_h = cv2.getTrackbarPos("LH", "Tracking")
+    # l_s = cv2.getTrackbarPos("LS", "Tracking")
+    # l_v = cv2.getTrackbarPos("LV", "Tracking")
+
+    # u_h = cv2.getTrackbarPos("UH", "Tracking")
+    # u_s = cv2.getTrackbarPos("US", "Tracking")
+    # u_v = cv2.getTrackbarPos("UV", "Tracking")
+
+    lower = np.array([78,0,142]) #For white range
+    upper = np.array([132,24,255])
+    
+    yellower = np.array([16,0,134]) #For yellow range
+    yelupper = np.array([64,177,255])
+    
+    yellowmask = cv2.inRange(hsv, yellower, yelupper)    
+    whitemask = cv2.inRange(hsv, lower, upper)
+
+
+    # l_b = np.array([l_h, l_s, l_v])
+    # u_b = np.array([u_h, u_s, u_v])
+
+    yellowmask = cv2.inRange(hsv, yellower, yelupper)    
+    whitemask = cv2.inRange(hsv, lower, upper)
+
+    mask = cv2.bitwise_or(yellowmask, whitemask)  
+    res = cv2.bitwise_and(frame, frame, mask = mask)   
+
+    # mask = cv2.inRange(hsv, l_b, u_b)
+
+    # res = cv2.bitwise_and(frame, frame, mask=mask)
+
+    cv2.imshow("frame", frame)
+    cv2.imshow("mask", mask)
+    cv2.imshow("res", res)
+
+    key = cv2.waitKey(1)
+    if key == 27:
+        break
+
+cv2.destroyAllWindows()
---
+++ NFS_SAMPLES/test3.py
@@ -0,0 +1,103 @@
+import cv2
+import numpy as np
+
+def nothing(x):
+    pass
+
+# white tracking
+cv2.namedWindow("WTracking")
+cv2.createTrackbar("WLH", "WTracking", 78, 255, nothing)
+cv2.createTrackbar("WLS", "WTracking", 0, 255, nothing)
+cv2.createTrackbar("WLV", "WTracking", 168, 255, nothing)
+
+cv2.createTrackbar("WUH", "WTracking", 132, 255, nothing)
+cv2.createTrackbar("WUS", "WTracking", 24, 255, nothing)
+cv2.createTrackbar("WUV", "WTracking", 255, 255, nothing)
+
+# yellow tracking
+cv2.namedWindow("YTracking")
+cv2.createTrackbar("YLH", "YTracking", 16, 255, nothing)
+cv2.createTrackbar("YLS", "YTracking", 0, 255, nothing)
+cv2.createTrackbar("YLV", "YTracking", 134, 255, nothing)
+
+cv2.createTrackbar("YUH", "YTracking", 64, 255, nothing)
+cv2.createTrackbar("YUS", "YTracking", 177, 255, nothing)
+cv2.createTrackbar("YUV", "YTracking", 255, 255, nothing)
+
+vidcap = cv2.VideoCapture("nfs1.mp4")
+# Capture video frame by frame
+success, frame = vidcap.read()
+
+
+while True:
+
+    # Capture video frame by frame
+    success, frame = vidcap.read()
+
+    # Resize the image frames
+    # frame = cv2.resize(frame, (640, 480))
+    frame = cv2.resize(frame, (1280, 720))
+
+    # frame = cv2.imread('nfs3.png')
+
+    hsv = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
+
+######### config
+    #white
+    w_l_h = cv2.getTrackbarPos("WLH", "WTracking")
+    w_l_s = cv2.getTrackbarPos("WLS", "WTracking")
+    w_l_v = cv2.getTrackbarPos("WLV", "WTracking")
+
+    w_u_h = cv2.getTrackbarPos("WUH", "WTracking")
+    w_u_s = cv2.getTrackbarPos("WUS", "WTracking")
+    w_u_v = cv2.getTrackbarPos("WUV", "WTracking")
+
+    #yellow
+    y_l_h = cv2.getTrackbarPos("YLH", "YTracking")
+    y_l_s = cv2.getTrackbarPos("YLS", "YTracking")
+    y_l_v = cv2.getTrackbarPos("YLV", "YTracking")
+
+    y_u_h = cv2.getTrackbarPos("YUH", "YTracking")
+    y_u_s = cv2.getTrackbarPos("YUS", "YTracking")
+    y_u_v = cv2.getTrackbarPos("YUV", "YTracking")
+
+    lower = np.array([w_l_h, w_l_s, w_l_v]) #For white range
+    upper = np.array([w_u_h, w_u_s, w_u_v])
+    
+    yellower = np.array([y_l_h, y_l_s, y_l_v]) #For yellow range
+    yelupper = np.array([y_u_h, y_u_s, y_u_v])
+
+########### end config
+
+    # lower = np.array([78,0,142]) #For white range
+    # upper = np.array([132,24,255])
+    
+    # yellower = np.array([16,0,134]) #For yellow range
+    # yelupper = np.array([64,177,255])
+    
+    yellowmask = cv2.inRange(hsv, yellower, yelupper)    
+    whitemask = cv2.inRange(hsv, lower, upper)
+
+
+    # l_b = np.array([l_h, l_s, l_v])
+    # u_b = np.array([u_h, u_s, u_v])
+
+    yellowmask = cv2.inRange(hsv, yellower, yelupper)    
+    whitemask = cv2.inRange(hsv, lower, upper)
+
+    mask = cv2.bitwise_or(yellowmask, whitemask)  
+    res = cv2.bitwise_and(frame, frame, mask = mask)   
+
+    # mask = cv2.inRange(hsv, l_b, u_b)
+
+    # res = cv2.bitwise_and(frame, frame, mask=mask)
+
+    # cv2.imshow("frame", frame)
+    # cv2.imshow("mask", mask)
+    cv2.imshow("res", res)
+
+    key = cv2.waitKey(1)
+    if key == 27:
+        break
+
+cv2.destroyAllWindows()

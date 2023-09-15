a324b4f33213b5d259040f49247bdc64d1badfc8
Enigma bell <54368594+jainsony@users.noreply.github.com>
2023-09-15 12:11:59 +0530

    Voice controlled robot car code added

---
+++ THIRD_EYE/qr_generator.py
@@ -0,0 +1,16 @@
+import qrcode
+
+img1 = qrcode.make('test_qr_1')
+img2 = qrcode.make('test_qr_2')
+img3 = qrcode.make('test_qr_3')
+img4 = qrcode.make('test_qr_4')
+
+print(type(img1))
+print(img1.size)
+# <class 'qrcode.image.pil.PilImage'>
+# (290, 290)
+
+img1.save('Images/test_qr_1.png')
+img2.save('Images/test_qr_2.png')
+img3.save('Images/test_qr_3.png')
+img4.save('Images/test_qr_4.png')
---
+++ Voice Controlled Car/python code/main.py
@@ -0,0 +1,66 @@
+import pyttsx3
+import speech_recognition as sr
+import time
+import serial
+
+control_string = ""
+control_string += str(0)+"," + str(0) +","+ str(0) +"," + str(0) + "\n"
+
+ser = serial.Serial(port='COM6',baudrate=115200,parity=serial.PARITY_NONE,stopbits=serial.STOPBITS_ONE,bytesize=serial.EIGHTBITS,timeout=0)
+
+engine=pyttsx3.init('sapi5')
+voices=engine.getProperty('voices')
+engine.setProperty('voices', voices[0].id)
+
+def speak(text):
+    engine.say(text)
+    engine.runAndWait()
+
+def takeCommand():
+    r=sr.Recognizer()
+    with sr.Microphone() as source:
+        print("Listening...")
+        audio=r.listen(source)
+        try:
+            print("Recognize.........")
+            query=r.recognize_google(audio, language='en-in')
+        except Exception as e:
+            print("Try Again.......")
+            return "None"
+        return query
+    
+def send_serial(go):
+        control_string = str(go) +","+ str(0)  +","+str(0)  +","+str(0)+"\n"
+        print(control_string)
+        ser.write(str.encode(control_string))
+        time.sleep(0.1)
+
+def main_call():
+    go = 0
+    while True:
+        query=takeCommand().lower()
+        if 'move forward' in query:
+            print("move forward...")
+            speak("moving forward...")
+            go = 1
+            send_serial(go)
+            
+        elif 'move backward' in query:
+            print("moving backward...")
+            speak("moving backward...")
+            go = 2
+            send_serial(go)
+            
+        elif 'stop' in query:
+            print("stop...")
+            speak("stopping the robot...")
+            go = 0
+            send_serial(go)
+
+        elif 'shutdown'or'exit' in query:
+            print("program shutdown...")
+            speak("I am shutting down the program...")
+            break
+
+if __name__=="__main__":
+    main_call()

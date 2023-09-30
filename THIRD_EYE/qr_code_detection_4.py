cbe1da1ea1ad0c0b2a616263bef0fc5170ae6bae
Enigma bell <54368594+jainsony@users.noreply.github.com>
2023-09-30 11:01:49 +0530

    new file added
    qr code detection 4

--- THIRD_EYE/qr_code_detection_4.py
+++ THIRD_EYE/qr_code_detection_4.py
@@ -12,6 +12,11 @@ y1=0
 x2=0
 y2=0
 
+tqx1=0
+tqy1=0
+tqx2=0
+tqy2=0 
+
 while True:
     ret, frame = cap.read()
 
@@ -31,23 +36,42 @@ while True:
                     color = (0, 255, 0)
                 else:
                     color = (0, 0, 255)
-                
+                              
                 x1 = int(p[0][0])
                 y1 = int(p[0][1])
 
                 x2 = int(p[2][0])
                 y2 = int(p[2][1])
 
+                # else:
+                #     tqx1=0
+                #     tqy1=0                     
+                #     tqx2=0
+                #     tqy2=0    
+
+                first_qr_point = (tqx1, tqy1)
+                second_qr_point = (tqx2, tqy2)
+
                 start_point = (x1, y1)
                 end_point = (x2, y2)
 
                 thickness = 3
-
+                
                 img = cv2.line(frame, start_point, end_point, color, thickness)
 
                 x, y = (x1 + x2) // 2, (y1 + y2) // 2
+
+                if s == 'test_qr_1':
+                    tqx1=x
+                    tqy1=y
+                
+                elif s == 'test_qr_4':
+                    tqx2=x
+                    tqy2=y
                 # Draw a circle in the center of rectangle
                 img = cv2.circle(img=img, center=(x, y), radius=3, color=(0, 0, 255), thickness=3)
+
+                img = cv2.line(frame, first_qr_point, second_qr_point, color, thickness)
                 # img = cv2.line(frame, P1, P3, color, thickness)
                 print("--------------")
                 # print(P1[0][0])
